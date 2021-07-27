a63711b195134c2d20622d9ca31d4eb6f3e73d68
promero <promero@mathworks.com>
2021-07-27 13:12:03 +0200

    Minor improvements in QNX toolchain file. Explicitly link libsocket for samples if building for QNX.
    Signed-off-by: promero <promero@mathworks.com>

--- cmake/toolchains/qnx.cmake
+++ cmake/toolchains/qnx.cmake
@@ -1,7 +1,10 @@
 set(CMAKE_SYSTEM_NAME QNX)
 
-set(arch gcc_ntox86_64)
+set(arch gcc_ntox86_64_gpp)
 set(ntoarch x86_64)
+
+set(CMAKE_SIZEOF_VOID_P 8)
+set(CMAKE_SYSTEM_PROCESSOR ${ntoarch})
 
 set(CMAKE_C_COMPILER qcc)
 set(CMAKE_C_COMPILER_TARGET ${arch})
@@ -11,7 +14,4 @@ set(CMAKE_CXX_COMPILER_TARGET ${arch})
 	
 set(CMAKE_SYSROOT $ENV{QNX_TARGET})
 
-set(CMAKE_CXX_FLAGS_INIT "-D_QNX_SOURCE -stdlib=libstdc++")
-
-# Explicitly link libraries if required
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-lsocket")
+set(CMAKE_CXX_FLAGS_INIT "-D_QNX_SOURCE")
