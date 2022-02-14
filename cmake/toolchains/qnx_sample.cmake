08dc8e72d8862a1cb38384b6263842e93c73f93b
Nicolas Dubois <nicolasdubois@not.an.email.net>
2022-02-14 14:58:06 +0100

    ED247LIB-36 - QNX toolchain sample

---
+++ cmake/toolchains/qnx_sample.cmake
@@ -0,0 +1,27 @@
+set(CMAKE_SYSTEM_NAME QNX)
+
+set(arch gcc_ntox86_64_gpp)
+set(ntoarch x86_64)
+
+set(CMAKE_SIZEOF_VOID_P 8)
+set(CMAKE_SYSTEM_PROCESSOR ${ntoarch})
+
+set(CMAKE_C_COMPILER qcc)
+set(CMAKE_C_COMPILER_TARGET ${arch})
+
+set(CMAKE_CXX_COMPILER q++)
+set(CMAKE_CXX_COMPILER_TARGET ${arch})
+	
+set(CMAKE_SYSROOT $ENV{QNX_TARGET})
+
+set(CMAKE_PREFIX_PATH ${CMAKE_SYSROOT}/${ntoarch}; ${CMAKE_SYSROOT}/${ntoarch}/usr)
+
+# libXML2
+#set(LibXml2_ROOT /)
+
+# GTest
+#set(GTest_ROOT /)
+
+# Doxygen
+#set(Doxygen_ROOT /)
+
