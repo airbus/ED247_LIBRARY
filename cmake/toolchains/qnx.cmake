dc0153658a8ea1302c417bf2165b1ba7deafc408
Pablo Romero <promero@mathworks.com>
2022-08-19 09:34:22 +0200

    Fixed QNX toolchain file.

--- cmake/toolchains/qnx.cmake
+++ cmake/toolchains/qnx.cmake
@@ -14,17 +14,11 @@ set(CMAKE_CXX_COMPILER_TARGET ${arch})
 	
 set(CMAKE_SYSROOT $ENV{QNX_TARGET})
 
+set(CMAKE_CXX_FLAGS_INIT "-D_QNX_SOURCE")
+set(CMAKE_NO_SYSTEM_FROM_IMPORTED TRUE)
+
+# Explicitly link libraries if required
+#set(CMAKE_EXE_LINKER_FLAGS_INIT "-lsocket")
+
+# Add additional paths if required
 set(CMAKE_PREFIX_PATH ${CMAKE_SYSROOT}/${ntoarch}; ${CMAKE_SYSROOT}/${ntoarch}/usr)
-
-# libXML2
-#set(LibXml2_ROOT /)
-
-# GTest
-#set(GTest_ROOT /)
-
-# Doxygen
-#set(Doxygen_ROOT /)
-
-# Simulink logger
-#set(SimulinkLogger_INCLUDE_DIR "/")
-
