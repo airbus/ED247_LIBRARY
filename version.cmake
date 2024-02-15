e176e9f791cbacfb27588d40f22fadf76e74efcc
Andrew Nikolayenkov <andrew.nikolayenkov@airbus.com>
2024-02-15 15:09:48 +0100

    ED247LIB-99 : Updated version.cmake to correct version

--- version.cmake
+++ version.cmake
@@ -1,3 +1,3 @@
 # Define the *target* version (note 'V' is not supported by CMake)
 # A 'real' version name will be computed based on git (aka git tag)
-set(CMAKE_PROJECT_VERSION 2.0.2)
+set(CMAKE_PROJECT_VERSION 2.0.3)
